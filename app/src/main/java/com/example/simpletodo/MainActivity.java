1f1b218efd0f4b5adfcaf584bdd67bb712acbe63
Nadin Tamer <nadint@fb.com>
2021-06-22 13:21:18 -0700

    Initial commit; basic UI setup complete

---
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.simpletodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<String> items;
+    Button buttonAdd;
+    EditText editTextItem;
+    RecyclerView recyclerViewItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        items = new ArrayList<>();
+        items.add("Buy milk");
+        items.add("Go to the gym");
+        items.add("Feed the cat");
+
+        buttonAdd = findViewById(R.id.buttonAdd);
+        editTextItem = findViewById(R.id.editTextItem);
+        recyclerViewItems = findViewById(R.id.recyclerViewItems);
+    }
+}
