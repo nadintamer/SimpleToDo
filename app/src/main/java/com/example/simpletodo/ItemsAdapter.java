9ccdacc48899b669f8f49a8420e18c72072162f6
Nadin Tamer <nadin.tamer@gmail.com>
2021-06-22 13:44:23 -0700

    UI displays list of todo items

---
+++ app/src/main/java/com/example/simpletodo/ItemsAdapter.java
@@ -0,0 +1,52 @@
+package com.example.simpletodo;
+
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
+    List<String> items;
+
+    public ItemsAdapter(List<String> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        return new ViewHolder(todoView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemsAdapter.ViewHolder holder, int position) {
+        String item = items.get(position);
+        holder.bind(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textViewItem;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewItem = itemView.findViewById(android.R.id.text1);
+        }
+
+        public void bind(String item) {
+            textViewItem.setText(item);
+        }
+    }
+}
--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.simpletodo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -22,13 +23,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        buttonAdd = findViewById(R.id.buttonAdd);
+        editTextItem = findViewById(R.id.editTextItem);
+        recyclerViewItems = findViewById(R.id.recyclerViewItems);
+
         items = new ArrayList<>();
         items.add("Buy milk");
         items.add("Go to the gym");
         items.add("Feed the cat");
 
-        buttonAdd = findViewById(R.id.buttonAdd);
-        editTextItem = findViewById(R.id.editTextItem);
-        recyclerViewItems = findViewById(R.id.recyclerViewItems);
+        ItemsAdapter itemsAdapter = new ItemsAdapter(items);
+        recyclerViewItems.setAdapter(itemsAdapter);
+        recyclerViewItems.setLayoutManager(new LinearLayoutManager(this));
     }
 }
