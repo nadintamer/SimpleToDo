8d067784c3c26ad0d7187d7c96076355e92d705d
Nadin Tamer <nadin.tamer@gmail.com>
2021-06-22 14:26:55 -0700

    Changes to item list persist even after app is closed

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -5,11 +5,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,19 +36,18 @@ public class MainActivity extends AppCompatActivity {
         editTextItem = findViewById(R.id.editTextItem);
         recyclerViewItems = findViewById(R.id.recyclerViewItems);
 
-        items = new ArrayList<>();
-        items.add("Buy milk");
-        items.add("Go to the gym");
-        items.add("Feed the cat");
+        loadItems();
 
         ItemsAdapter.onLongClickListener onLongClickListener = new ItemsAdapter.onLongClickListener() {
             @Override
             public void onItemLongClicked(int position) {
                 items.remove(position);
                 itemsAdapter.notifyItemRemoved(position);
+                saveItems();
                 Toast.makeText(getApplicationContext(), "To-do successfully removed!", Toast.LENGTH_SHORT).show();
             }
         };
+
         itemsAdapter = new ItemsAdapter(items, onLongClickListener);
         recyclerViewItems.setAdapter(itemsAdapter);
         recyclerViewItems.setLayoutManager(new LinearLayoutManager(this));
@@ -54,8 +59,30 @@ public class MainActivity extends AppCompatActivity {
                 items.add(newItem);
                 itemsAdapter.notifyItemInserted(items.size() - 1);
                 editTextItem.setText("");
+                saveItems();
                 Toast.makeText(getApplicationContext(), "To-do successfully added!", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private File getDataFile() {
+        return new File(getFilesDir(), "data.txt");
+    }
+
+    private void loadItems() {
+        try {
+            items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error reading items", e);
+            items = new ArrayList<>();
+        }
+    }
+
+    private void saveItems() {
+        try {
+            FileUtils.writeLines(getDataFile(), items);
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error writing items", e);
+        }
     }
 }
